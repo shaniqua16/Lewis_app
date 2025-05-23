4c4f83d54a115054723f4b252e4c65534ad72244
shaniqua16 <shaniquawhitley@yahoo.com>
2025-05-22 21:54:53 -0500

    Next .JS

---
+++ Map.js
@@ -0,0 +1,13 @@
+// <!-- PROBLEM:
+    
+// APP FUNCTION:
+//     -Display photos
+//     -list sevices provided
+    
+// ADMIN FUNCTION:
+
+
+// USER FUNCTION:
+//     -User needs to be able to see services offered
+//     -User needs to be able to leave a review and read othr reviews
+//  -->
